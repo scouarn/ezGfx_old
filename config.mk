0dad74f7e52841a4c5f870657dce47c27d9ba8a8
scouarn <scouarn.contact@gmail.com>
2021-12-04 10:44:18 +0100

    makefile config

--- config.mk
+++ config.mk
@@ -1,88 +1,31 @@
-# HOST := UNIX 
-# HOST := WIN
-
-# TARGET := UNIX
-# TARGET := WIN
 
 CFLAGS := -O3 -Wall -Iinclude
 
 
-
-ifndef HOST
-	ifeq ($(OS),Windows_NT)
-		HOST := WIN
-	else
-		HOST := UNIX
-	endif 
-else 
-	#remove white spaces
-	HOST := $(strip $(HOST))
-endif
-
-
-ifndef TARGET
-	TARGET := $(HOST)
-endif
-
-
-
-#undefine default CC value
-CC =
-
-ifeq ($(TARGET),UNIX)
-
-	CORE := ezGfx_core_linux_xorg
-	LIB := bin/libezgfx.so
-
-	LIBS := -lX11 -lpthread
-	OBJFLAGS := -c -fPIC
-	LIBFLAGS := -shared
-
-	ifeq ($(HOST),UNIX)
-		CC := gcc
-	endif #no windows-to-linux crosscompiler ?
-
-
-else ifeq ($(TARGET),WIN)
-
-	CORE := ezGfx_core_win_gdi
-	LIB := bin/libezgfx.dll
+ifeq ($(OS),Windows_NT)
+	
+	CC := mingw32-gcc
 
 	LIBS := -lgdi32
 	OBJFLAGS := -c -DBUILD_DLL
 	LIBFLAGS := -shared
 
-	ifeq ($(HOST),UNIX)
-		CC := x86_64-w64-mingw32-gcc #put your crosscompiler here
+	CORE := ezGfx_core_win_gdi
+	LIB := bin/libezgfx.dll
 
-	else ifeq ($(HOST),WIN)
-		CC := mingw32-gcc
+	forceremove = del /F /Q $(subst /,\\, $(1))
 
-	endif
+else 
 
-endif
+	CC := gcc
 
+	LIBS := -lX11 -lpthread
+	OBJFLAGS := -c -fPIC
+	LIBFLAGS := -shared
 
-#defining the correct command for cleaning
+	CORE := ezGfx_core_linux_xorg
+	LIB := bin/libezgfx.so
 
-ifeq ($(HOST),UNIX)
 	forceremove = rm -rf $(1)
 
-else ifeq ($(HOST),WIN)
-	forceremove = del /F /Q $(subst /,\\, $(1))
-endif
-
-
-
-#check if most things are correct
-ifndef CC
-$(error No compiler defined)
-endif
-
-ifndef LIB
-$(error No target lib file defined)
-endif
-
-ifndef CORE
-$(error No core implementation defined)
 endif
